0cb6aaf13b767fcb5c76d3def8ca6da826b1c017
maxkrishkapachal <60788817+maxkrishkapachal@users.noreply.github.com>
2020-02-27 17:27:48 -0700

    Update Lower.java

--- Lower.java
+++ Lower.java
@@ -4,14 +4,14 @@ public class Lower extends Grids{
 	public char type;
 	public Bombs1 game;
 	public int gameSpaces;
-	public ArrayList bombSquad = new ArrayList();
+	public ArrayList bombSquad = new ArrayList(36);
 	
 	public Lower(char level) {
 		this.type = level;
 		this.game = new Bombs1(this.type);
 	}
 	
-	public ArrayList createBoard() {
+	public ArrayList createLowerBoard() {
 		ArrayList c = game.Bombs();
 		gameSpaces = (int)(game.getRowSize() * game.getRowSize());
 		
@@ -22,7 +22,7 @@ public class Lower extends Grids{
 		for (int x = 0; x < c.size(); x += 2) {
 			int rs = game.getRowSize();
 			int y = x + 1;
-			int bombLocation = ((int)c.get(x) - 1) * (rs) + ((int)c.get(y));
+			int bombLocation = ((int)c.get(x) - 1) * (rs) + ((int)c.get(y) - 1);
 			bombSquad.set(bombLocation, "[B]");
 		}
 		
