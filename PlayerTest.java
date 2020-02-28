d3ac12497b9625d14d7b1adf83e6e402975d68cb
nathanielgarcia <60788867+nathanielgarcia@users.noreply.github.com>
2020-02-28 10:57:08 -0700

    Add files via upload

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,15 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
 	public static void main(String[] args) {
 		Player c = new Player();
-		c.Move();
+		Grids  lower = new Grids();
+		lower.Grids('N');
+		ArrayList lowergrid = lower.setLowerBoard();
+		Grids  upper = new Grids();
+		upper.Grids('N');
+		ArrayList uppergrid= upper.setLowerBoard();
+		c.Move(lowergrid);
 
 	}
 
 }
-		
-
-
-
-
