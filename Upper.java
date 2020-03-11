2cd23b45780239c3001d679456b81d56f17f1e19
maxkrishkapachal <60788817+maxkrishkapachal@users.noreply.github.com>
2020-03-11 16:39:00 -0600

    Add files via upload

---
+++ Upper.java
@@ -0,0 +1,17 @@
+package application;
+import java.util.ArrayList;
+
+public class Upper extends Grids{
+	private ArrayList<String> bombSquad = new ArrayList<String>(super.getGameSpaces());
+	
+	public Upper(int aLevel) {
+		super(aLevel);
+	}
+
+	public ArrayList buildUpperBoard() {
+		for (int i = 0; i < super.getGameSpaces(); i++){
+			bombSquad.add("[ ]");
+		}
+		return bombSquad;
+	}
+}
